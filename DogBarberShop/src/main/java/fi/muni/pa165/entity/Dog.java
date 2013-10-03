ceb29d9e089ad0f84fd6ca9663e1586f4dadf7eb
grcko <grcko@GrckoHP>
2013-10-03 19:26:44 +0200

    vytvorenie entitnej triedy Dog

---
+++ DogBarberShop/src/main/java/fi/muni/pa165/entity/Dog.java
@@ -0,0 +1,74 @@
+package fi.muni.pa165.entity;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Oliver Pentel
+ */
+public class Dog implements Serializable {
+    private Long id;
+    private String name;
+    private String breed;
+    private int age;
+
+    public Dog(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 11 * hash + (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Dog other = (Dog) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+}
