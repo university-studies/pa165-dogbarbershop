5d51d5ac174bd7333ba3d2cf6f2af0b46fe7138d
Pavol Loffay <p.loffay@gmail.com>
2013-10-30 13:58:19 +0100

    Employee, pridana trieda na prevody Entity to DTO

---
+++ DogBarberShop/src/main/java/fi/muni/pa165/utils/EmployeeConvertor.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.muni.pa165.utils;
+
+import fi.muni.pa165.dto.EmployeeDto;
+import fi.muni.pa165.entity.Employee;
+
+/**
+ *
+ * @author Pavol Loffay <p.loffay@gmail.com>
+ */
+public class EmployeeConvertor {
+    public static EmployeeDto EmployeeToEmployeeDto(Employee empE)
+    {
+        EmployeeDto empDto = new EmployeeDto();
+        
+        empDto.setId(empE.getId());
+        empDto.setName(empE.getName());
+        empDto.setSurname(empE.getSurname());
+        empDto.setPhone(empE.getPhone());
+        empDto.setSalary(empE.getSalary());
+        empDto.setAddress(empE.getAddress());
+                
+        return empDto;
+    }
+    
+    public static Employee EmployeeDtoToEmployee(EmployeeDto empDto) {
+        Employee empE = new Employee();
+        
+        empE.setId(empDto.getId());
+        empE.setName(empDto.getName());
+        empE.setSurname(empDto.getSurname());
+        empE.setAddress(empDto.getAddress());
+        empE.setPhone(empDto.getPhone());
+        empE.setSalary(empDto.getSalary());
+        
+        return empE;
+    }
+}
