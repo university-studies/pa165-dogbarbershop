47a89a0f89c308d7574b91494418143d9fe6ed6c
Pavol Loffay <p.loffay@gmail.com>
2013-11-01 20:57:45 +0100

    Employee service, priprava testu

--- DogBarberShop/src/test/java/fi/muni/pa165/service/EpmloyeeServiceTest.java
+++ DogBarberShop/src/test/java/fi/muni/pa165/service/EpmloyeeServiceTest.java
@@ -4,9 +4,10 @@
  */
 package fi.muni.pa165.service;
 
+import fi.muni.pa165.dto.EmployeeDto;
 import fi.muni.pa165.idao.EmployeeDao;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
 
 /**
  *
@@ -14,10 +15,20 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class EpmloyeeServiceTest {
  
-    @Autowired
+    @Mock
     EmployeeDao empDaoMock; 
+    
+    /*
+     * Important! This needs to be somewhere in the base class or a test runner:
+     * MockitoAnnotations.initMocks(testClass);
+     */
+    
     
     @Test
     public void testAddEmployee() {
+        EmployeeDto empDto = new EmployeeDto(new Long(0), "Pavol", "Loffay", "Zapad 1144",
+                "090526955", "15666");
+        
+        
     }
 }
