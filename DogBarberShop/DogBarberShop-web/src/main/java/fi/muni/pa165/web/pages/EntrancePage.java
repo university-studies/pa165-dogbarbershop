c6f41ec90192e8a52d30dcf052519b238c2e7bb5
grcko <grcko@GrckoHP>
2013-11-22 18:36:01 +0100

    - entrancePage

--- DogBarberShop/DogBarberShop-web/src/main/java/fi/muni/pa165/web/pages/EntrancePage.java
+++ DogBarberShop/DogBarberShop-web/src/main/java/fi/muni/pa165/web/pages/EntrancePage.java
@@ -1,49 +1,60 @@
 package fi.muni.pa165.web.pages;
 
-import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.markup.html.link.StatelessLink;
+import fi.muni.pa165.dto.DogDto;
+import fi.muni.pa165.dto.ServiceDto;
+import fi.muni.pa165.service.DogService;
+import fi.muni.pa165.service.DogServiceService;
+import fi.muni.pa165.service.EmployeeService;
+import fi.muni.pa165.service.ServiceService;
+import fi.muni.pa165.web.DogBarberShopApplication;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.Model;
 
 /**
- * @author Oliver Pentek, Jan Pacner, martin
+ * @author Oliver Pentek
  */
-public class EntrancePage extends TemplatePage {
+public final class EntrancePage extends TemplatePage {
+    private final ServiceService serviceService;
+    private final DogService dogService;
+    private final DogServiceService dogServiceService;
+    
     public EntrancePage() {
         super();
+        serviceService = DogBarberShopApplication.get().getServiceService();
+        dogService = DogBarberShopApplication.get().getDogService();
+        dogServiceService = DogBarberShopApplication.get().getDogServiceService();
+        
         this.initComponents();
     }
     
     private void initComponents() {
+        final DropDownChoice<DogDto> dogDropDownChoice = new DropDownChoice(ComponentIDs.DOG_DROP_DOWN_CHOICE, new Model<DogDto>(),dogService.getAllDogs());
+        final DropDownChoice<ServiceDto> serviceDownChoice = new DropDownChoice(ComponentIDs.SERVICE_DROP_DOWN_CHOICE, new Model<ServiceDto>(), serviceService.getAllServices());
+        final Form addDogService = new Form (ComponentIDs.ADD_DOG_SERVICE_FORM) {
+
+            @Override
+            protected void onSubmit() {
+                super.onSubmit();
+                dogServiceService.createDogService(dogDropDownChoice.getModelObject(), serviceDownChoice.getModelObject(), null);
+            }
+            
+        };
+        this.add(addDogService);
+        addDogService.add(new Button(ComponentIDs.SUBMIT_BUTTON));
         
-        this.add(new Link<EmployeePage>(ComponentIDs.EMPLOYEE_LINK) {
-            @Override
-            public void onClick() {
-                this.setResponsePage(EmployeePage.class);
-            }
-        });
-        this.add(new Link(ComponentIDs.A_DOG) {
-            @Override
-            public void onClick() {
-                this.getRequestCycle().setResponsePage(DogPage.class);
-            }
-        });
-        this.add(new Link(ComponentIDs.A_CUSTOMER) {
-            @Override
-            public void onClick() {
-                this.getRequestCycle().setResponsePage(CustomerPage.class);
-            }
-        });
-        this.add(new Link(ComponentIDs.SERVICE_LINK) { 
-            @Override
-            public void onClick() {
-                this.getRequestCycle().setResponsePage(ServicePage.class);
-            }
-        });
+        
+        addDogService.add(dogDropDownChoice);
+        addDogService.add(serviceDownChoice);
+        
+        
     }
     
     private static class ComponentIDs {
-        private static final String EMPLOYEE_LINK = "employeeLink";
-        private static final String A_DOG = "a_dog";
-        private static final String A_CUSTOMER = "a_customer";
-        private static final String SERVICE_LINK = "serviceLink";
+        private static final String ADD_DOG_SERVICE_FORM = "addDogServiceForm";
+        private static final String DOG_DROP_DOWN_CHOICE = "dogDropDownChoice";
+        private static final String SERVICE_DROP_DOWN_CHOICE = "serviceDropDownChoice";
+        private static final String SUBMIT_BUTTON = "submitButton";
     }
 }
